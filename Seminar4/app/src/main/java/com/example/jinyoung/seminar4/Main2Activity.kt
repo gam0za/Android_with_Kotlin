a5a4989ee995cc62f0ad890134875964d100f400
DESKTOP-BQOE09A\Jinyoung <gam0za@naver.com>
2017-11-13 02:30:02 +0900

    아맞다 탭 커스텀

--- Seminar4/app/src/main/java/com/example/jinyoung/seminar4/Main2Activity.kt
+++ Seminar4/app/src/main/java/com/example/jinyoung/seminar4/Main2Activity.kt
@@ -11,10 +11,10 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        tab.addTab(tab.newTab().setText("1번 탭"))
-        tab.addTab(tab.newTab().setText("2번 탭"))
-        tab.addTab(tab.newTab().setText("3번 탭"))
-        tab.addTab(tab.newTab().setText("4번 탭"))
+        tab.addTab(tab.newTab().setText("1번 탭").setIcon(resources.getDrawable(R.drawable.zzanggu1)))
+        tab.addTab(tab.newTab().setText("2번 탭").setIcon(resources.getDrawable(R.drawable.zzanggu1)))
+        tab.addTab(tab.newTab().setText("3번 탭").setIcon(resources.getDrawable(R.drawable.zzanggu1)))
+        tab.addTab(tab.newTab().setText("4번 탭").setIcon(resources.getDrawable(R.drawable.zzanggu1)))
 
         var tabAdapter = TabAdapter(supportFragmentManager,tab.tabCount)
 
@@ -25,10 +25,14 @@ class Main2Activity : AppCompatActivity() {
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    tab.setIcon(resources.getDrawable(R.drawable.zzanggu1))
+                }
             }
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 viewPager.currentItem=tab!!.position
+                tab.setIcon(resources.getDrawable(R.drawable.zzanggu2))
             }
         })
     }
